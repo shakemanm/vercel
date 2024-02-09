6880386e5d6de8ddfa4eecb1fecda27d53e2bb69
shakemanm <54585829+shakemanm@users.noreply.github.com>
2024-02-09 07:41:51 +0200

    gitignore

--- mtn/settings.py
+++ mtn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5a*x_n%e25#b-r^&uk97w#8ultvx%gk&i044io%fcu2u%yzi29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # '.vercel.app'
 
--- mtn/wsgi.py
+++ mtn/wsgi.py
@@ -15,4 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtn.settings')
 
 application = get_wsgi_application()
 
+app = application
+
 
